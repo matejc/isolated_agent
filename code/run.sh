82a609204f36148784b8c6e0e21edfd6b41a0585
Matej Cotman <matej@matejc.com>
2026-08-11 00:36:49 +0300

    feat(code): add new agent

---
+++ code/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+docker rm -f isolated_code || true
+docker build "$SCRIPT_DIR" -t isolated_code
+docker run --rm --name isolated_code \
+    -v "$SCRIPT_DIR/state:/root/.code" \
+    -v "${1?"Missing first argument: Path to workdir!"}:/workspace" \
+    "${@:2}" \
+    -it isolated_code
